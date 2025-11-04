d1aff9de95488d0d660199dfe73d7a809aab54fb
Sadeem Alharthi <121146237+SadeemHarthi@users.noreply.github.com>
2025-11-04 11:18:55 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,15 +1,15 @@
 WITH investor_totals AS (
-    SELECT investor_id, SUM(no_of_shares) AS total_shares
-    FROM investor_transactions
-    GROUP BY investor_id
+   SELECT investor_id, SUM(no_of_shares) AS total_shares
+   FROM investor_transactions
+   GROUP BY investor_id
 )
-SELECT 
-    t.investor_id,
-    s.sector_name,
-    ROUND(t.no_of_shares * 100.0 / it.total_shares, 1) AS share_percentage
-FROM investor_transactions AS t
-JOIN investor_totals AS it
-    ON t.investor_id = it.investor_id
-JOIN sectors AS s
-    ON t.sector_id = s.sector_id
-ORDER BY t.investor_id, share_percentage;
+SELECT
+   t.investor_id,
+   s.sector_name,
+   ROUND( (t.no_of_shares * 1.0 / it.total_shares) * 100 , 2 ) AS percentage
+FROM investor_transactions t
+JOIN investor_totals it
+   ON t.investor_id = it.investor_id
+JOIN sectors s
+   ON t.sector_id = s.sector_id
+ORDER BY t.investor_id, percentage;
